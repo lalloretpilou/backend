269babf578c40532fb6beb25fe55a3d885410eca
pilou <pierrelouis95lalloret@gmail.com>
2022-07-13 19:04:08 +0200

    add backend folder

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const userRoutes = require('./routes/user.routes');
+const app = express();
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+app.use('/api/auth', userRoutes);
+module.exports = app;
---
+++ routes/user.routes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const userCtrl = require('../controllers/user.controller')
+
+router.post('/signup',function (req, res) {userCtrl.signup});
+router.post('/login', function (req, res) {userCtrl.login});
+
+module.exports = router;
